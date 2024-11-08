9d33b1fe5cddd55ef66bc32f5e677759c48d860b
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-11-08 08:43:21 +0000

    Añadimos las historias y los títulos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,10 @@
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -16,42 +18,55 @@ import androidx.compose.ui.window.rememberWindowState
 
 @Composable
 fun App() {
-    Column(modifier = Modifier.padding(15.dp)) {
-        accounts.forEach { account ->
-            Card(
-                Modifier
-                    .size(width = 400.dp, height = 120.dp)
-                    .padding(vertical = 10.dp),
-                backgroundColor = Color.LightGray,
-                contentColor = Color.White,
-                elevation = 10.dp
-            ) {
-                Row(modifier = Modifier.padding(10.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp)
+    ) {
+        Text(
+            text = "Fabio's Instagram",
+            fontSize = TextUnit(value = 35f, type = TextUnitType.Sp)
+        )
+        Text(
+            text = "Historias",
+            fontSize = TextUnit(value = 20f, type = TextUnitType.Sp)
+        )
+
+        Row(
+            modifier = Modifier
+                .padding(15.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(15.dp)
+        ) {
+            accounts.forEach { account ->
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.width(80.dp)
+                ) {
                     Image(
                         modifier = Modifier
-                            .size(50.dp)
-                            .clip(CircleShape),
+                            .size(80.dp)
+                            .clip(CircleShape)
+                            .border(2.dp, Color.Red, CircleShape),
                         painter = painterResource(resourcePath = account.image),
                         contentDescription = "Foto de ${account.name}"
                     )
-                    Column(modifier = Modifier.padding(start = 10.dp)) {
-                        Text(account.name, style = MaterialTheme.typography.subtitle1)
-                        Text(
-                            modifier = Modifier.padding(top = 5.dp),
-                            text = "Lorem ipsum",
-                            style = MaterialTheme.typography.body2
-                        )
-                    }
+                    Text(
+                        text = account.name,
+                        style = MaterialTheme.typography.subtitle2,
+                        color = Color.Black,
+                        modifier = Modifier.padding(top = 5.dp)
+                    )
                 }
             }
         }
         Text(
-            text = "Form",
-            modifier = Modifier.padding(top = 10.dp),
+            text = "Publicaciones",
             fontSize = TextUnit(value = 20f, type = TextUnitType.Sp)
         )
     }
 }
+
 
 fun main() = application {
     Window(
--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -1,23 +1,17 @@
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
-
-
 data class Account (val name: String, val image: String)
-
-const val body = "Lorem ipsum dolor si asjfojsdoif jdsiofjdiosfj"
-
 val accounts = listOf<Account>(
-    Account("cagarro_futbol", "img1.jpg"),
-    Account("iriemdaoscuridad", "img2.jpg"),
-    Account("ferprogamer23", "img3.jpg"),
-    Account("unaigatitagangster", "img4.jpg"),
-    Account("simongoofy", "img5.jpg")
+    Account("cagarro_futbol", "amaro.jpg"),
+    Account("iriemdaoscuridad", "iriem.jpg"),
+    Account("ferprogamer23", "fer.jpg"),
+    Account("unaigatitagangster", "unai.jpg"),
+    Account("simongoofy", "simon.jpg")
 )
 
-val items = listOf(
-    "Home" to Icons.Default.Home,
-    "Search" to Icons.Default.Search,
-    "Settings" to Icons.Default.Settings
+data class Post (val postImage: String, val authorIcon: String, val authorName: String, val description: String)
+val posts = listOf<Account>(
+    Post("amaro.jpg","cagarro_futbol", "amaro.jpg"),
+    Post("iriemdaoscuridad", "iriem.jpg"),
+    Post("ferprogamer23", "fer.jpg"),
+    Post("unaigatitagangster", "unai.jpg"),
+    Post("simongoofy", "simon.jpg")
 )
