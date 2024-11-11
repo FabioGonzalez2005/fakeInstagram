b95807c40e645586601b4e2d6c12ecaa56dafe1f
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-11-11 13:55:42 +0000

    Actualizamos

--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -12,3 +12,4 @@ val posts = listOf<Post>(
     Post("postUnai.jpg","unai.jpg", "unaigatitagangster", "Hoy he destrozado a Laura en mi partida de Clash Royale, además mi vida a cambiado para mejor, esto se debe a que FER SE HA IDO DEL CLAN #adiosfer #lauraeresmalísima #soylider"),
     Post("postFer.jpg","fer.jpg", "ferprogamer23", "Fotos de mi última competición, los envidiosos dirán que no soy yo #mrolimpia #cbummepatrocina"),
 )
+
